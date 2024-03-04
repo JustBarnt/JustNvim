71e3ca5be1ccd1924ff7329b6d1f23259a1e8a2a
justbarnt <functnal@gmail.com>
2024-03-04 16:44:41 -0500

    feat: added Activate.lua

--- lua/plugins/dbee.lua
+++ lua/plugins/dbee.lua
@@ -1,9 +1,18 @@
 return {
     "kndndrj/nvim-dbee",
+    tag = "v0.1.0",
     dependencies = {
-        "muniftanjim/nui.nvim",
+        {
+            "muniftanjim/nui.nvim",
+            commit = "35da9ca1de0fc4dda96c2e214d93d363c145f418",
+        },
     },
     config = function()
-        require('dbee').setup({})
-    end
+        require("dbee").setup({
+            sources = vim.list_extend(require("dbee.config").default.sources, {
+                require("dbee.sources").EnvSource:new "DBEE_CONNECTIONS",
+                require("dbee.sources").FileSource:new(vim.fn.stdpath "cache" .. "/dbee/persistence.json"),
+            }),
+        })
+    end,
 }
