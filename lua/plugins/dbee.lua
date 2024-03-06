e2c4c6bae2ab699b7648a2a0e0eae71053eed043
justbarnt <functnal@gmail.com>
2024-03-06 17:11:07 -0500

    feat: GOT DBEE WORKING STUPID CLOCK OS

--- lua/plugins/dbee.lua
+++ lua/plugins/dbee.lua
@@ -1,18 +1,21 @@
 return {
-    "kndndrj/nvim-dbee",
-    tag = "v0.1.0",
-    dependencies = {
-        {
-            "muniftanjim/nui.nvim",
-            commit = "35da9ca1de0fc4dda96c2e214d93d363c145f418",
-        },
-    },
+    -- "kndndrj/nvim-dbee",
+    "justbarnt/nvim-dbee",
+    dependencies = { "muniftanjim/nui.nvim" },
     config = function()
-        require("dbee").setup({
-            sources = vim.list_extend(require("dbee.config").default.sources, {
-                require("dbee.sources").EnvSource:new "DBEE_CONNECTIONS",
-                require("dbee.sources").FileSource:new(vim.fn.stdpath "cache" .. "/dbee/persistence.json"),
-            }),
+        local sources = require "dbee.sources"
+        local dbee = require "dbee"
+        local layout = require("dbee.layouts").Default
+        dbee.setup({
+            sources = {
+                sources.MemorySource:new({
+                    {
+                        name = "CommSys",
+                        type = "sqlserver",
+                        url = "sqlserver://sa:admin@localhost?database=CommSys&TrustServerCertificate=true",
+                    },
+                }),
+            },
         })
     end,
 }
