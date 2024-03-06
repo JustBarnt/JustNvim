06a6d7aae0cfe2c58dfdd6a505d3f8d3a6aebaaf
justbarnt <functnal@gmail.com>
2024-03-06 14:18:15 -0500

    chore: more `.norg` specific opts

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -2,4 +2,5 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.breakat = " ^I!@*-+;:,./?"
 vim.opt.breakindent = true
-
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
