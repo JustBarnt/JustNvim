ef7d9c2170713330130f5d8aaceba5b6fef0fd6e
justbarnt <functnal@gmail.com>
2024-03-19 16:11:02 -0400

    fixed: undo tree on windows

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-if vim.fn.has("win32") then
+if vim.fn.has "win32" then
     local ori_fnameescape = vim.fn.fnameescape
     ---@diagnostic disable-next-line: duplicate-set-field
     vim.fn.fnameescape = function(...)
@@ -13,16 +13,6 @@ end
 
 if not vim.g.vscode then
     require "core"
-    if vim.g.loaded_undotree then
-        vim.g.undotree_WindowLayout = 2
-
-        local os_name = jit.os
-        if os_name == 'Windows' then
-            vim.g.undotree_DiffCommand="FC"
-        elseif os_name == 'Linux' then
-            vim.g.undotree_DiffCommand="diff"
-        end
-    end
 else
     require("core.keymaps").Default()
 end
